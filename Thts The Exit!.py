092388e2105664e632e1a31df9e2578d2dc59995
Aadya-1188 <aadyapathak2509@gmail.com>
2025-10-04 17:48:19 +0530

    Add files via upload
    exit()

---
+++ Thts The Exit!.py
@@ -0,0 +1,10 @@
+#Python program to demonstrate exit()
+for i in range(10):
+    #If the value of i becomes 5 , then program is forced to exit!
+    if i == 5:
+        #Printing the exit mesage
+        print(exit)
+        exit()  
+    print(i)
+
+    
